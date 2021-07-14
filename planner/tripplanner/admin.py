040d7f2f11580466cb9f6cc32334c4426905698e
generic <ericxieau@gmail.com>
2021-07-14 20:46:52 +1000

    Added scheduling for the calender and displayed it on the admin site

--- planner/tripplanner/admin.py
+++ planner/tripplanner/admin.py
@@ -1,3 +1,8 @@
+from __future__ import unicode_literals
 from django.contrib import admin
+from .models import Event
+ 
+admin.site.register(Event)
 
-# Register your models here.
+class EventAdmin(admin.ModelAdmin):
+    list_display = ['day', 'start_time', 'end_time', 'notes']
